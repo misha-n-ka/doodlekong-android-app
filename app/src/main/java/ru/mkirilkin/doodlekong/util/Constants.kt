c1225159240eba0684aaab990f9765b1b1cfd299
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-11-17 09:08:06 +0700

    create Constants with base url fields

---
+++ app/src/main/java/ru/mkirilkin/doodlekong/util/Constants.kt
@@ -0,0 +1,9 @@
+package ru.mkirilkin.doodlekong.util
+
+object Constants {
+
+    const val USE_LOCALHOST = true
+
+    const val HTTP_BASE_URL = ""
+    const val HTTP_BASE_URL_LOCALHOST = "http://192.168.1.34:8001/"
+}
