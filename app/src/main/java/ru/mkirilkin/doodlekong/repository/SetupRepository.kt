c62b5fe0f6dca90b9577b9ab007c1daa65781770
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-11-17 09:46:56 +0700

    create setup repository and its default implementation

---
+++ app/src/main/java/ru/mkirilkin/doodlekong/repository/SetupRepository.kt
@@ -0,0 +1,13 @@
+package ru.mkirilkin.doodlekong.repository
+
+import ru.mkirilkin.doodlekong.data.remote.websocket.Room
+import ru.mkirilkin.doodlekong.util.Resource
+
+interface SetupRepository {
+
+    suspend fun createRoom(room: Room): Resource<Unit>
+
+    suspend fun getRooms(searchQuery: String): Resource<List<Room>>
+
+    suspend fun joinRoom(username: String, roomName: String): Resource<Unit>
+}
